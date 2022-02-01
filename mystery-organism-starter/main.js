17b8f8365115d618e2ebc630dc9b9d2721132938
axelwood <axelwood1996@gmail.com>
2022-01-31 22:57:41 -0300

    Revert "Change how the variable is added again x 2"
    This reverts commit 192733ffe7ec48088e39364ace36af9090c19b4c.

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -97,7 +97,7 @@ const willsurv = array =>{
   let counter = 0
   for(let i=0; i< array.length; i++){
     if(array[i]=== 'C' || array[i]=== 'G'){
-      counter += 1
+      counter += 10
     }
 
   }
